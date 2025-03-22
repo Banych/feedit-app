d746bee500991b075529c5cca4a0a8ed5b1e563e
Vlad Banykin <banykinv@gmail.com>
2025-03-22 21:08:55 +0100

    add link on the post body in the feed, add loading state for vote component buttons, add link to user on post details

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,27 +1,30 @@
-import { cn } from '@/lib/utils';
-import { cva, VariantProps } from 'class-variance-authority';
-import { Loader2 } from 'lucide-react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
+import { cn } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
+
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 active:scale-95 disabled:pointer-events-none disabled:opacity-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default: 'bg-zinc-900 text-zinc-100 hover:bg-zinc-800',
-        destructive: 'text-white hover:bg-red-600 dark:hover:bg-red-600',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'bg-zinc-100 text-zinc-900 outline outline-1 outline-zinc-300 hover:bg-zinc-200',
-        subtle: 'bg-zinc-100 text-zinc-900 hover:bg-zinc-200',
-        ghost:
-          'bg-transparent text-zinc-800 hover:bg-zinc-100 data-[state=open]:bg-transparent',
-        link: 'bg-transparent text-slate-900 underline-offset-4 hover:bg-transparent hover:underline dark:bg-transparent dark:text-slate-100 dark:hover:bg-transparent',
+          'border border-input bg-background text-secondary-foreground hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-2',
-        xs: 'h-8 rounded-sm px-1.5',
+        sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
+        icon: 'size-10',
       },
     },
     defaultVariants: {
@@ -31,6 +34,53 @@ const buttonVariants = cva(
   }
 );
 
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  isLoading?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      variant,
+      size,
+      isLoading,
+      children,
+      asChild = false,
+      disabled,
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        disabled={isLoading || disabled}
+        {...props}
+      >
+        {isLoading ? (
+          <Loader2
+            className={cn(
+              'mr-2 size-5 animate-spin',
+              isLoading && size === 'icon' && 'mr-0'
+            )}
+          />
+        ) : null}
+        {isLoading && size === 'icon' ? null : children}
+      </Comp>
+    );
+  }
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
+
+/*
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
@@ -52,6 +102,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     );
   }
 );
-Button.displayName = 'Button';
-
-export { Button, buttonVariants };
+*/
